4bd21d3ac69323e9ed1539cca266337c96a34edd
Musaddiq Habib <msdqhabib@gmail.com>
2024-04-05 17:53:23 +0500

    commetn down the logs for now

--- project/production.py
+++ project/production.py
@@ -16,30 +16,30 @@ DATABASES = {
 }
 
 
-# Logging Configuration
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "file": {
-            "level": "INFO",
-            "class": "logging.FileHandler",
-            "filename": "/home/ubuntu/Task-Management-System/error.log",
-        },
-    },
-    "loggers": {
-        "django": {
-            "handlers": ["file"],
-            "level": "INFO",
-            "propagate": True,
-        },
-        "api_integration": {
-            "handlers": ["file"],
-            "level": "INFO",
-            "propagate": True,
-        }
-    },
-}
+# # Logging Configuration
+# LOGGING = {
+#     "version": 1,
+#     "disable_existing_loggers": False,
+#     "handlers": {
+#         "file": {
+#             "level": "INFO",
+#             "class": "logging.FileHandler",
+#             "filename": "/home/ubuntu/Task-Management-System/error.log",
+#         },
+#     },
+#     "loggers": {
+#         "django": {
+#             "handlers": ["file"],
+#             "level": "INFO",
+#             "propagate": True,
+#         },
+#         "api_integration": {
+#             "handlers": ["file"],
+#             "level": "INFO",
+#             "propagate": True,
+#         }
+#     },
+# }
 
 
 ALLOWED_HOSTS = ['*']
