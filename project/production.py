6269aa278e64c89384b28baafeea2add3a035e86
Musaddiq Habib <msdqhabib@gmail.com>
2024-04-05 17:44:47 +0500

    add the postgresql db setup for production

--- project/production.py
+++ project/production.py
@@ -4,5 +4,42 @@ import os
 
 DEBUG = False
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangodb',
+        'HOST': 'localhost',
+        'PORT': '',
+        'USER': 'ubuntu',
+        'PASSWORD': 'ubuntu',
+    }
+}
+
+
+# Logging Configuration
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "/home/ubuntu/Fulfillkaro-Portal/cron.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True,
+        },
+        "api_integration": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True,
+        }
+    },
+}
+
 
 ALLOWED_HOSTS = ['*']
